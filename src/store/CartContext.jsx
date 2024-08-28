9110c126a7547b1cad49cfb3cc0c394317f6c2ef
mohamuhsin <muhsinohoho@outlook.com>
2024-08-28 12:59:21 +0300

    used the cartContext data in MealItem where we accessed addItem and in Header component were we accessed the cart.items

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -9,13 +9,13 @@ const CartContext = createContext({
 function cartReducer(state, action) {
     if (action.type === "ADD_ITEM") {
         const existingCartItemIndex = state.items.findIndex(
-            (item) => item.id == action.id
+            (item) => item.id == action.item.id
         );
 
         const updatedItems = [...state.items];
 
         if (existingCartItemIndex > -1) {
-            const existingItem = state.item[existingCartItemIndex];
+            const existingItem = state.items[existingCartItemIndex];
             const updatedItem = {
                 ...existingItem,
                 quantity: existingItem.quantity + 1,
@@ -31,7 +31,7 @@ function cartReducer(state, action) {
 
     if (action.type === "REMOVE_ITEM") {
         const existingCartItemIndex = state.items.findIndex(
-            (item) => item.id == action.item.id
+            (item) => item.id == action.id
         );
 
         const existingCartItem = state.items[existingCartItemIndex];
@@ -51,7 +51,7 @@ function cartReducer(state, action) {
 
     return state;
 }
-export function CartContextProvider() {
+export function CartContextProvider({ children }) {
     const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
     function addItem(item) {
@@ -73,6 +73,8 @@ export function CartContextProvider() {
         addItem,
         removeItem,
     };
+
+    console.log(cartContext);
 
     return (
         <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
