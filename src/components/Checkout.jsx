728b69fc235e7c3b4946c10dab5ff25b510e81db
mohamuhsin <muhsinohoho@outlook.com>
2024-08-29 18:45:16 +0300

    created custom input component, used it in the cart, close the checkout modal, and opened it, solved the probelms with modal visibility

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,46 @@
+import Modal from "./Modal";
+import { currencyFormatter } from "../util/formatting";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+
+export default function Checkout() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+    const cartTotal = cartCtx.items.reduce(
+        (totalPrice, item) => totalPrice + item.quantity * item.price,
+        0
+    );
+
+    function handleCloseCheckout() {
+        userProgressCtx.hideCheckout();
+    }
+
+    return (
+        <Modal
+            open={userProgressCtx.progress === "checkout"}
+            onClose={handleCloseCheckout}
+        >
+            <form>
+                <h2>Your Checkout</h2>
+                <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="E-Mail Address" type="email" id="email" />
+                <Input label="Street" type="text" id="street" />
+            </form>
+            <div className="control-row">
+                <Input label="postal-code" type="text" id="postal-code" />
+                <Input label="City" type="text" id="city" />
+            </div>
+
+            <p className="modal-actions">
+                <Button type="button" textOnly onClick={handleCloseCheckout}>
+                    Close
+                </Button>
+                <Button>Submit Order</Button>
+            </p>
+        </Modal>
+    );
+}
