d63200260e4974ce64b2ecac18fe236fa86911ba
mohamuhsin <muhsinohoho@outlook.com>
2024-08-29 17:11:50 +0300

    showed our checkout button conditionally only if we got items in the cart

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -64,7 +64,7 @@ export function CartContextProvider({ children }) {
     function removeItem(id) {
         dispatchCartAction({
             type: "REMOVE_ITEM",
-            payload: id,
+            id,
         });
     }
 
